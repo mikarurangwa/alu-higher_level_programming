537ed8e81220e065ed63eb51de10eee9ac3f19b1
mikarurangwa <r.mika@alustudent.com>
2024-07-13 06:22:23 +0200

    Update 100-magic_string.py

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 def magic_string():
     magic_string.n = getattr(magic_string, 'n', 0) + 1
-    return ("BestSchool, " * (magic_string.n - 1) + "BestSchool")
+    return ", ".join(["BestSchool"] * magic_string.n)
